7724e1c36a64d741d1b156a6d15f34e0e2c033b0
andriyanto <andrimoh7@gmail.com>
2019-12-06 08:42:03 +0700

    dashboard

---
+++ app/src/main/java/id/co/iconpln/smartcity/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,14 @@
+package id.co.iconpln.smartcity.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.smartcity.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+
+}
