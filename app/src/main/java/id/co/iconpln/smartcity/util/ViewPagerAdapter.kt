b47a9e715b6c459d243a591fb43cd301ebf7795d
Muhammad Faisal Rizki <mfrizki1@gmail.com>
2020-02-06 09:22:49 +0700

    adding ViewPagerAdapter

---
+++ app/src/main/java/id/co/iconpln/smartcity/util/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package id.co.iconpln.smartcity.util
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+    var fragmentList = arrayListOf<Fragment>()
+    var titleList = arrayListOf<String>()
+
+    fun populateFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+
+    override fun getItem(position: Int) = fragmentList[position]
+
+    override fun getCount() = fragmentList.size
+
+    override fun getPageTitle(position: Int) = titleList[position]
+}
